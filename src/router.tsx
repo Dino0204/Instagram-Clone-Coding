79091f10bb3a398c920a49a552c43577575e1bc1
Dino0204 <s24020@gsm.hs.kr>
2025-04-15 09:13:37 +0900

    Feat: 라우팅

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Main, Error } from "./views"
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ } />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   )
