78ff8de8273b9fd97df42561b1841957d497f9ac
Dino0204 <s24020@gsm.hs.kr>
2025-04-18 01:19:04 +0900

    🐛 Change: Mocks -> Unsplash API Data

--- src/views/main/ui/index.tsx
+++ src/views/main/ui/index.tsx
@@ -1,25 +1,28 @@
-import { Header, Post } from "../../../widgets"
-import { getAllPosts } from "../../../shared/apis/getAllPosts"
+import { Header, ImgPost } from "../../../widgets"
+import { getAllImages } from "../../../shared/apis/getAllPosts"
 import { useQuery } from "@tanstack/react-query"
-import { PostProps } from "../../../widgets/post/model"
 import { useState } from "react"
+import { UnsplashPhoto } from "../../../widgets/imgPost/model"
 
 export function Main() {
-  const { data: posts } = useQuery<PostProps[]>({ queryKey: ["posts"], queryFn: getAllPosts })
+  const { data: imgs } = useQuery<UnsplashPhoto[]>({ queryKey: ["imgs"], queryFn: getAllImages })
   const [searchQuery, setSearchQuery] = useState("")
 
-  const results = posts?.filter((post) =>
-    post.caption.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    post.user.username.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    post.location?.name.toLowerCase().includes(searchQuery.toLowerCase())
+  const results = imgs?.filter((img) =>
+    img.description?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    img.user.username.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    img.alt_description?.toLowerCase().includes(searchQuery.toLowerCase())
   )
 
   return (
     <div className="flex flex-col ">
       <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <main className="flex flex-col w-full items-center gap-2 py-5">
-        {results?.map((post) => (
+        {/* {results?.map((post) => (
           <Post key={post.id} {...post} />
+        ))} */}
+        {results?.map((img) => (
+          <ImgPost key={img.id} {...img} />
         ))}
       </main>
     </div>
