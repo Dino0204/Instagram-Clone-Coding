11d7b293a910be5bcaba1c165574d07c57b92cbe
Dino0204 <s24020@gsm.hs.kr>
2025-04-21 17:33:57 +0900

    🐛 Fix: 검색 기능 수정

--- src/shared/utils/searchResult.tsx
+++ src/shared/utils/searchResult.tsx
@@ -5,11 +5,10 @@ interface PostsProps {
 }
 
 export const SearchResult = (posts: PostsProps, searchQuery: string) => {
-  const results = posts?.pages[0].filter((post: UnsplashPhoto) =>
+  const results = posts?.pages.flat().filter((post: UnsplashPhoto) =>
     post.description?.toLowerCase().includes(searchQuery.toLowerCase()) ||
     post.user.username.toLowerCase().includes(searchQuery.toLowerCase()) ||
     post.alt_description?.toLowerCase().includes(searchQuery.toLowerCase())
   );
-
   return results;
 };
