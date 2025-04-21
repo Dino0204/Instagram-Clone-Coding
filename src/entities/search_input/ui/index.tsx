de2ece6a1b935f17da96df9b46833f5d54d7bd7b
Dino0204 <s24020@gsm.hs.kr>
2025-04-21 17:56:21 +0900

    🐛 Fix: Enter

--- src/entities/search_input/ui/index.tsx
+++ src/entities/search_input/ui/index.tsx
@@ -5,14 +5,16 @@ import { SearchProps } from "../../../shared/types/search";
 export function SearchInput({ setSearchQuery }: SearchProps) {
   const [inputValue, setInputValue] = useState("")
 
-  const handleChange = (value: string) => {
-    setInputValue(value)
-    setSearchQuery(value)
-  }
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      setSearchQuery(inputValue);
+      event.preventDefault();
+    }
+  };
 
   return (
     <form className="flex items-center p-2 border rounded-xl bg-gray-100 ">
-      <input className="w-full outline-0" value={inputValue} onChange={(e) => handleChange(e.target.value)} />
+      <input className="w-full outline-0" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKeyDown} />
       <button className="cursor-pointer">
         <Search width={32} />
       </button>
