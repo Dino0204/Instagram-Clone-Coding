af5b5b2f2c141409649cffb15cf3a06010a7d1be
Dino0204 <s24020@gsm.hs.kr>
2025-04-21 17:33:45 +0900

    Feat: React Query를 통한 무한 스크롤 기능 구현

--- src/views/main/ui/index.tsx
+++ src/views/main/ui/index.tsx
@@ -2,32 +2,54 @@ import { Header, Post } from "../../../widgets"
 import { getAllImagesByPage } from "../../../shared/apis/getAllPosts"
 import { UnsplashPhoto } from "../../../widgets/post/model"
 import { useInfiniteQuery } from "@tanstack/react-query"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
+import { useInterSectionsObserver } from "../../../shared/hooks/useInterSectionsObserver"
 import { SearchResult } from "../../../shared/utils/searchResult"
 
 export function Main() {
+  const { data: posts, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ['posts'],
+    queryFn: ({ pageParam }) => getAllImagesByPage({ pageParam }),
+    getNextPageParam: (lastPage, allPages) => {
+      return allPages.length + 1;
+    },
+    initialPageParam: 1
+  });
   const [searchQuery, setSearchQuery] = useState("")
   const [result, setResult] = useState<UnsplashPhoto[]>([])
-  const { data: posts } = useInfiniteQuery({
-    queryKey: ['posts'],
-    queryFn: getAllImagesByPage,
-    initialPageParam: 0,
-    getNextPageParam: (lastPage) => lastPage.nextCursor
-  });
+  const { observe, unobserve } = useInterSectionsObserver(() => fetchNextPage())
+  const target = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     if (posts) setResult(SearchResult(posts, searchQuery))
   }, [posts, searchQuery])
 
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    const currentTarget = target.current;
+
+    return () => {
+      if (currentTarget && hasNextPage && !isFetchingNextPage) {
+        console.log("타겟:", currentTarget);
+        observe(currentTarget);
+      }
+      else if (currentTarget && !hasNextPage) {
+        console.log("타겟 해제:", currentTarget);
+        unobserve(currentTarget)
+      }
+    }
+  }, [hasNextPage, isFetchingNextPage, observe, unobserve])
+
   return (
     <div className="flex flex-col ">
       <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <main className="flex flex-col w-full items-center gap-2 py-5">
-        {result?.map((post: UnsplashPhoto) => (
-          <Post key={post.id} {...post} />
+        {result?.map((post: UnsplashPhoto, index) => (
+          <Post key={index} {...post} />
         ))}
-      </main>
-    </div>
+        <div className="w-full h-8 text-center" ref={target}>{isFetchingNextPage ? "로딩중..." : "로딩완료!"}</div>
+      </main >
+    </div >
   )
 }
 
